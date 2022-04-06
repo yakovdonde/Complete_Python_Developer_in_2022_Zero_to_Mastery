358709c3025ecbe766dd94e8b87bd251cd94eb03
yakovdonde <yakovdonde@gmail.com>
2022-04-06 16:35:20 +0300

    commit

--- Password Checker.py
+++ Password Checker.py
@@ -1,8 +1,14 @@
 # Password checker
 
+# The goals are:
+# 1 - printing the username
+# 2 - printing the hidden password in a form of astrix
+# 3 - printing the password length
 
-username = (input('Hi, please enter your username \n'))
-password = (input('Hi, please enter your password \n'))
-password_length = "*" * len(password)
 
-print(f"Hey {username}, your password of {password_length} is {len(password)} characters long ")
+username = (input('Hi, please enter your username: \n'))
+password = (input('Hi, please enter your password: \n'))
+password_masked = "*" * len(password)
+password_length = len(password)
+
+print(f"Hey {username}, your password of {password_masked} is {password_length} characters long ")
