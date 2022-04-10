d8fd9b6af802f884b55090d047091fa23376a524
yakovdonde <yakovdonde@gmail.com>
2022-04-10 18:02:50 +0300

    commit

---
+++ Tricky Counter.py
@@ -0,0 +1,8 @@
+# A counter to count the items in a list.
+# Use loop to iterate over the list and sum up the total of the list
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+sum = 0
+
+for d in my_list:
+    sum +=d
+print(sum)
